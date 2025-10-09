409747e68cb23c604dbbca2039c279d872b090d8
Taka6116 <goto_takashi@cellmuller.com>
2025-10-09 10:47:59 +0900

    Update 自己紹介サイト.js

--- 自己紹介サイト.js
+++ 自己紹介サイト.js
@@ -1,11 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5QV8TKR67C"></script> 
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-5QV8TKR67C');
-</script>
 
 <script>
     // ハンバーガーメニューのトグル
@@ -48,3 +40,4 @@
         appearOnScroll.observe(fader);
     });
 </script>
+
